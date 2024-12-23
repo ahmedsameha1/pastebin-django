a02ca768e2ed7a324c10d3bd7c2077179d24af3a
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2024-12-23 15:59:00 +0200

    Create end2end test for getting back the pastebin text using the url

--- end2end_tests.py
+++ end2end_tests.py
@@ -36,6 +36,20 @@ class NewVisitorTest(unittest.TestCase):
         self.assertEqual(header_url.text,
                          "http://localhost:8000/6bde0a9108435670b29d23fd689a0e90")
 
+    def test_visit_the_pastebin_using_url(self):
+        self.browser.get("http://localhost:8000")
+        inputbox = self.browser.find_element(By.ID, "id_new_pastebin")
+        inputbox.send_keys("This is a test pastebin")
+        submit_button = self.browser.find_element(By.TAG_NAME, "button")
+        submit_button.click()
+        current_url = self.browser.current_url
+        self.assertEqual(current_url,
+                         "http://localhost:8000/6bde0a9108435670b29d23fd689a0e90")
+        url_header = self.browser.find_element(By.TAG_NAME, "h1").text
+        self.assertEqual(url_header, "http://localhost:8000/6bde0a9108435670b29d23fd689a0e90")
+        pastebin_text = self.browser.find_element(By.TAG_NAME, "p").text
+        self.assertEqual(pastebin_text, "This is a test pastebin")
+        
 
 if __name__ == "__main__":
     unittest.main()
--- pb/views.py
+++ pb/views.py
@@ -5,7 +5,9 @@ import hashlib
 
 def home_page(request):
     if request.method == "POST":
-        return render(request, "home.html", {"pastebin_url": "http://localhost:8000/" +
-                            hashlib.md5(request.POST["pastebin_text"]
-                                        .encode("utf-8")).hexdigest()})
+        return render(request,
+                      "home.html",
+                      {"pastebin_url": "http://localhost:8000/" +
+                       hashlib.md5(request.POST["pastebin_text"]
+                                   .encode("utf-8")).hexdigest()})
     return render(request, "home.html")
