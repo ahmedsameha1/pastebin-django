3a2d6cb2af501dbe19b616072c2afb875bda28a7
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2024-12-23 15:15:33 +0200

    Submitting a text show a url

--- pb/tests.py
+++ pb/tests.py
@@ -2,7 +2,14 @@ from django.test import TestCase
 from django.http import HttpRequest
 from pb.views import home_page
 
+
 class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
+
+    def test_that_a_POST_request_shows_a_url(self):
+        response = self.client.post("/", data={"pastebin_text": "hi"})
+        self.assertTemplateUsed(response, "home.html")
+        self.assertContains(response,
+                            "http://localhost:8000/49f68a5c8493ec2c0bf489821c21fc3b")
--- pb/views.py
+++ pb/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import hashlib
+
 
 def home_page(request):
+    if request.method == "POST":
+        return render(request, "home.html", {"pastebin_url": "http://localhost:8000/" +
+                            hashlib.md5(request.POST["pastebin_text"]
+                                        .encode("utf-8")).hexdigest()})
     return render(request, "home.html")
