8ebb3663e522e620ac2ed4d226ebeeb0438a2e99
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2024-12-24 18:10:34 +0200

    Model for pastebin and associated migration

--- pb/models.py
+++ pb/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Pastebin(models.Model):
+    text = models.TextField(default="")
+    id = models.TextField(default="", primary_key=True)
--- pb/tests.py
+++ pb/tests.py
@@ -1,5 +1,7 @@
+import hashlib
 from django.test import TestCase
 from django.http import HttpRequest
+from pb.models import Pastebin
 from pb.views import home_page
 
 
@@ -13,3 +15,32 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, "home.html")
         self.assertContains(response,
                             "http://localhost:8000/49f68a5c8493ec2c0bf489821c21fc3b")
+
+
+class PastebinModelTest(TestCase):
+    def test_saving_and_retrieving_pastebins(self):
+        first_pastebin = Pastebin()
+        first_pastebin.text = "one"
+        first_pastebin.id = hashlib.md5("one"
+                                        .encode("utf-8")).hexdigest()
+        first_pastebin.save()
+
+        second_pastebin = Pastebin()
+        second_pastebin.text = "two"
+        second_pastebin.id = hashlib.md5("two"
+                                         .encode("utf-8")).hexdigest()
+        second_pastebin.save()
+
+        saved_items = Pastebin.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_pastebin = saved_items[0]
+        second_saved_pastebin = saved_items[1]
+        self.assertEqual(first_saved_pastebin.text, "one")
+        self.assertEqual(first_saved_pastebin.id,
+                         hashlib.md5("one"
+                                     .encode("utf-8")).hexdigest())
+        self.assertEqual(second_saved_pastebin.text, "two")
+        self.assertEqual(second_saved_pastebin.id,
+                         hashlib.md5("two"
+                                     .encode("utf-8")).hexdigest())
