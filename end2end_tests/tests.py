3b3dd014fb0760fb1d5a68ddbccbb5f9a6358d9d
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2024-12-31 20:20:41 +0200

    Delete a pastebin after visiting it

--- end2end_tests/tests.py
+++ end2end_tests/tests.py
@@ -6,12 +6,12 @@ import time
 
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Chrome()
 
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_create_a_pastebin_then_visiting_it(self):
+    def test_can_create_a_pastebin_then_visiting_it_then_delete_it(self):
         self.browser.get(self.live_server_url)
         self.assertIn("Pastebin", self.browser.title)
         header_text = self.browser.find_element(By.TAG_NAME, "h1").text
@@ -37,3 +37,25 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertEqual(url_header, "6bde0a9108435670b29d23fd689a0e90")
         pastebin_text = self.browser.find_element(By.TAG_NAME, "p").text
         self.assertEqual(pastebin_text, "This is a test pastebin")
+        delete_button = self.browser.find_element(By.TAG_NAME, "button")
+        self.assertEqual(delete_button.get_attribute("type"),
+                         "submit")
+        self.assertEqual(delete_button.text, "Delete")
+        delete_button.click()
+        time.sleep(1)
+        confirm = self.browser.switch_to.alert
+        self.assertEqual(confirm.text, "Delete?")
+        confirm.accept()
+        time.sleep(1)
+        alert = self.browser.switch_to.alert
+        self.assertEqual(alert.text, "Deleted")
+        current_url = self.browser.current_url
+        self.assertEqual(current_url, self.live_server_url)
+        self.browser.get(self.live_server_url+"/6bde0a9108435670b29d23fd689a0e90")
+        time.sleep(1)
+        alert = self.browser.switch_to.alert
+        self.assertEqual(alert.text, "Invalid id")
+        alert.accept()
+        time.sleep(1)
+        current_url = self.browser.current_url
+        self.assertEqual(current_url, self.live_server_url)
